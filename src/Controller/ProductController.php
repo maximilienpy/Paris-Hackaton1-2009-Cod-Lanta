89ac41b2c72c1dbf59a249ad9e66dd3d55ea7879
ALI AFZAL <layaafzal74@gmail.com>
2020-11-19 23:44:34 +0100

    3push

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,28 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 11/10/17
- * Time: 16:07
- * PHP version 7
- */
 
 namespace App\Controller;
-
 use App\Model\ProductManager;
-use App\Model\ExchangeManager;
-use App\Model\SizeManager;
-use App\Model\ArtistManager;
-use App\Model\ImageManager;
-
+use App\Model\PictureManager;
 /**
  * Class ProductController
  *
  */
 class ProductController extends AbstractController
 {
-
-
     /**
      * Display product listing
      *
@@ -34,12 +20,9 @@ class ProductController extends AbstractController
     public function index()
     {
         $productManager = new ProductManager();
-        $products = $productManager->selectAll();
-
+        $products = $productManager->selectAllWithDetail();
         return $this->twig->render('Product/index.html.twig', ['products' => $products]);
     }
-
-
     /**
      * Display product informations specified by $id
      *
@@ -52,16 +35,9 @@ class ProductController extends AbstractController
     public function show(int $id)
     {
         $productManager = new ProductManager();
-        $product = $productManager->selectOneById($id);
-
-        $images = $productManager->selectAllImgByProduct($id);
-
-        return $this->twig->render('Product/show.html.twig', [
-            'product' => $product,
-            ]);
+        $product = $productManager->selectOneWithDetails($id);
+        return $this->twig->render('Product/show.html.twig', ['product' => $product]);
     }
-
-
     /**
      * Display product edition page specified by $id
      *
@@ -74,11 +50,45 @@ class ProductController extends AbstractController
     public function edit(int $id): string
     {
         $productManager = new ProductManager();
-        $product = $productManager->selectOneById($id);
-
-        $exchangeManager = new ExchangeManager();
-        $categories = $exchangeManager->selectAll();
-
-        $ownerManager = new OwnerManager();
-        $owner = $ownerManager->selectAll();
-
+        $product = $productManager->selectOneWithDetails($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $product = [
+                'id' => $_POST['id'],
+                'name' => $_POST['name'],
+                'function' => $_POST['function'],
+                'quantity' => $_POST['quantity'],
+                'picture' => $_POST['picture'],
+                'durability' => $_POST['durability'],
+                'owner_id' => $_POST['owner_id'],
+            ];
+            $productManager->update($product);
+            header('Location:/product/show/' . $id);
+        }
+        return $this->twig->render('Product/edit.html.twig', [
+            'product' => $product
+        ]);
+    }
+    /**
+     * Display specie creation page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function add()
+    {
+ 
+    }
+    /**
+     * Handle product deletion
+     *
+     * @param int $id
+     */
+    public function delete(int $id)
+    {
+        $productManager = new ProductManager();
+        $productManager->delete($id);
+        header('Location:/product/index');
+    }
+}
